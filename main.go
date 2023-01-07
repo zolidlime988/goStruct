c43af28511e844f02ffd0ee1a57c27773583f108
Apiwat Supaksirakol <api.supak@gmail.com>
2023-01-07 15:12:10 +0700

    assign value to struct

--- main.go
+++ main.go
@@ -3,6 +3,10 @@ package main
 import "fmt"
 
 func main() {
-	p1 := person{firstName: "Alex", lastName: "Anderson"}
+	var p1 person
 	fmt.Println(p1)
+	fmt.Printf("%+v\n", p1) // print value + key
+	p1.firstName = "John"
+	p1.lastName = "Doe"
+	fmt.Printf("%+v\n", p1) // print value + key
 }
